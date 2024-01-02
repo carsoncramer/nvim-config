7d6b15ab5a18dd81accebd65c583f5655033fb82
carsoncramer <carsoncramer286@gmail.com>
2024-01-02 12:34:42 -0700

    Changed colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,6 +6,16 @@ local M = {
 }
 
 M.name = "tokyonight-night"
+--[[
+local M = {
+  "lunarvim/horizon.nvim",
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+}
+
+M.name = "horizon"
+]]
+
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
